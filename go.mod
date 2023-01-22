f9b4cabdb93724798c1504002aa7d25c618a4b29
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-22 12:40:36 -0300

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/josethz00/go-datastructures
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World to Go Data Structures project!")
+}
