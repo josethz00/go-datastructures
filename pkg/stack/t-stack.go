db77569e470c36db02ee26f54d5de517dc7239c5
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-23 16:31:48 -0300

    feat(): create stack pkg

---
+++ pkg/stack/t-stack.go
@@ -0,0 +1,54 @@
+package t_stack
+
+import "fmt"
+
+type Stack struct {
+	max   int
+	top   int
+	value []int
+}
+
+func NewStack(max int) *Stack {
+	return &Stack{
+		max:   max,
+		top:   -1,
+		value: make([]int, max),
+	}
+}
+
+func (s *Stack) isEmpty() bool {
+	return s.top == -1
+}
+
+func (s *Stack) isFull() bool {
+	return s.top == s.max-1
+}
+
+func (s *Stack) Push(newValue int) {
+	if s.isFull() {
+		return
+	}
+
+	s.top++
+	s.value[s.top] = newValue
+}
+
+func (s *Stack) Pop() int {
+	if s.isEmpty() {
+		return -1
+	}
+
+	value := s.value[s.top]
+	s.top--
+
+	return value
+}
+
+func (s *Stack) Top() int {
+	if s.isEmpty() {
+		fmt.Println("Stack is empty")
+		return -1
+	}
+
+	return s.value[s.top]
+}
